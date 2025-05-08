eaacf4405208e7402b5d53de687a180c197beb29
Martin <martindealmeida17@gmail.com>
2025-05-08 16:16:52 -0300

    feat: componente link

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import Banner from "./components/Banner"
+import Banner from "./components/Banner";
+import Link from "./components/Link";
 
 function App() {
-  return <Banner src="./Imagens/Banner_Desktop.png" alt="Banner"/>;
-  
+  return (
+    <>
+      <Banner src="./Imagens/Banner_Desktop.png" alt="Banner" />;
+      <Link href="/" target="_blank">Link</Link>
+    </>
+  );
 }
 
-export default App
+export default App;
